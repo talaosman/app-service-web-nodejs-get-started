9964f33e8ea65f18ca70177de95fa98fb9d0b0ee
Tala osman <talaosman180@gmail.com>
2025-11-22 14:42:43 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'My Clinic Portal' });
+});
+
+router.get('/about', function(req, res) {
+  res.render('about', { title: 'About' });
+});
+
+router.get('/contact', function(req, res) {
+  res.render('contact', { title: 'Contact' });
+});
+
+router.post('/contact', function(req, res) {
+  // Here you can handle contact form submissions
+  res.send('Thank you for contacting us!');
 });
 
 module.exports = router;
